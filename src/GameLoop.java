3f3ab69f9fd7ab566885b2237866b7a96166619e
Misoka <afagernes@bredband.net>
2015-02-02 14:29:16 +0100

    Added "run" and "init" methods
    Game loop and initilization

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -1,4 +1,45 @@
+import java.awt.*;
+import javax.swing.JFrame;
 
-public class GameLoop {
-
+public class GameLoop extends JFrame{
+	
+	private boolean isRunning;
+	private static final int fps = 60;
+	
+	public void run()
+	{
+		
+		init();
+		
+		while(isRunning)
+		{
+			long time = System.currentTimeMillis();
+			
+			update();
+			render();
+			
+			time = (1000 / fps) - (System.currentTimeMillis() - time);
+			
+			if(time > 0)
+			{
+				try
+				{
+					Thread.sleep(time);
+				}
+				catch(Exception e){}
+			}
+		}
+		
+		setVisible(false);
+	}
+	
+	private void init()
+	{
+		setTitle("Pewpewvideogeim");
+		setSize(1280, 720);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
 }
