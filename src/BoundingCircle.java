eb2c7c7f4fcef136b6bcb455733add8ef1117a78
Joan4329 <Joand91@gmail.com>
2015-02-05 20:52:59 +0100

    Renamed to BoundingCircle

--- src/BoundingCircle.java
+++ src/BoundingCircle.java
@@ -1,22 +1,22 @@
 //import java.awt.Image;
 
 // Used by PhysicsEngine to perform collision checks. When creating new GameObject subclasses make this an attribute if collision is wanted for object. 
-public class BoundingSphere {
+public class BoundingCircle {
 	private Vector center;
 	private double radius;
 	
-	public BoundingSphere(double r){
+	public BoundingCircle(double r){
 		center = new Vector();
 		radius = r;
 	}
 	
-	public BoundingSphere(double r, double offsetX, double offsetY){
+	public BoundingCircle(double r, double offsetX, double offsetY){
 		center = new Vector(offsetX, offsetY);
 		radius = r;
 	}
 	
 //	Maybe this?
-//	public BoundingSphere(double r, Image img){
+//	public BoundingCircle(double r, Image img){
 //		center = new Vector(img.getWidth(ImageObserver)/2, img.getHeight(ImageObserver)/2)
 //	}
 	
