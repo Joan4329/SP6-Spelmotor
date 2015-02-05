7c17f3c2bdf3e8c4b2e2e4e9c04976171edf8609
Misoka <afagernes@bredband.net>
2015-02-05 20:55:13 +0100

    Added AABB, AABB vs AABB and AABB vs Circle collision

--- src/PhysicsEngine.java
+++ src/PhysicsEngine.java
@@ -13,11 +13,49 @@ public class PhysicsEngine {
 		
 		return false;
 	}
-	public static boolean chechBoxCollision(AABB a, AABB b){
+	
+	public static boolean checkBoxCollision(AABB a, AABB b){
 		
 		boolean test = (a.getXMax() < b.getXMax() || (b.getXMax() < a.getXMin()) ||
 						a.getYMax() < b.getYMin() || (b.getYMax() < a.getYMin()));
 		
 		return !test;
 	}
+	
+	public static boolean checkCircleAABB(BoundingCircle circle, AABB box){
+		double sqDist = sqDistPointAABB(circle.getVector(), box);
+		double r = circle.getRadius();
+		
+		return sqDist <= r * r;
+	}
+	
+	public static double sqDistPointAABB(Vector p, AABB aabb){
+		double sqDist = 0.0f;
+		double v;
+		double minX, minY, maxX, maxY;
+		
+		minX = aabb.getXMin();
+		minY = aabb.getYMin();
+		maxX = aabb.getXMax();
+		maxY = aabb.getYMax();
+		
+		v = p.getX();
+		
+		if (v < minX)
+			sqDist += (minX - v) * (minX - v);
+		
+		if (v > maxX)
+			sqDist += (v - maxX) * (v - maxX);
+		
+		v = p.getY();
+		
+		if (v < minY)
+			sqDist += (minY - v) * (minY - v);
+		
+		if (v > maxY)
+			sqDist += (v - maxY) * (v - maxY);
+		
+		return sqDist;
+	}
+	
 }
